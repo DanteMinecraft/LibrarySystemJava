c49247f9a36d5d45ce3c150220868e36625641bd
DanteMinecraft <dantewallen07@gmail.com>
2026-05-07 22:19:27 +0200

    added LibraryManager to the project

---
+++ src/main/java/net/dante/LibraryManager.java
@@ -0,0 +1,15 @@
+package net.dante;
+
+import java.util.ArrayList;
+
+import net.dante.items.BookItem;
+import net.dante.items.MagazineItem;
+
+public class LibraryManager {
+    // Variabels
+    private ArrayList<LibraryItem> items;
+    private ArrayList<BookItem> bookItems;
+    private ArrayList<MagazineItem> magazineItems;
+    //private ArrayList<User> users;
+    //private ArrayList<SuspendedUser> suspendedUser;
+}
