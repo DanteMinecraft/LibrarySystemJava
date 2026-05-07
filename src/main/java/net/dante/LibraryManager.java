077d287527d3ed82ad72c6b087726e7adc3ba2cf
DanteMinecraft <dantewallen07@gmail.com>
2026-05-07 22:46:44 +0200

    lade till basic structure inuti min LibraryManager

--- src/main/java/net/dante/LibraryManager.java
+++ src/main/java/net/dante/LibraryManager.java
@@ -16,4 +16,28 @@ public class LibraryManager {
     //private ArrayList<SuspendedUser> suspendedUser;
 
     private Gson gson = new Gson();
+
+
+    // Server URL handling
+    private String serverUrl;
+
+    public LibraryManager(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    // Method for fetching magazines
+    public void fetchBooks() {
+
+    }
+
+    // Method for fetching magazines
+    public void fetchMagazines() {
+
+    }
+
+    // Method for adding item to library
+    public void addLibraryItem(LibraryItem libItem) {
+
+    }
+    
 }
--- src/main/java/net/dante/Main.java
+++ src/main/java/net/dante/Main.java
@@ -3,14 +3,24 @@ package net.dante;
 public class Main {
     void main() {
 
+        LibraryManager Manager = new  LibraryManager();
+
+        while (true) { 
+
             IO.println("""
             -----Biblioketsystem-----
-            1. Lista bibliotekets böcker
-            2. Lista bibliotekets magasin
-            3. Registrera ny bok i systemet
-            4. Registrera nytt magasin i systemet
-            5. Avsluta programmet
+            1. Hämta data för samtliga böcker
+            2. Hämta data för samtliga magasin
+            3. Lista bibliotekets böcker och magasin
+            4. Registrera ny bok i systemet
+            5. Registrera nytt magasin i systemet
+            6. Avsluta programmet
         """);
-        
+
+        switch (IO.readln()) {
+            case 1 -> 
+            }
+            
+        }
     }
 }
