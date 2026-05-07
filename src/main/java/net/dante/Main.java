452ec57d4076f9e9aa1ad272f7f1481a1fae3259
DanteMinecraft <dantewallen07@gmail.com>
2026-05-07 22:25:36 +0200

    skapade visuell meny (saknar funktionalitet för närvarande)

--- src/main/java/net/dante/Main.java
+++ src/main/java/net/dante/Main.java
@@ -2,6 +2,16 @@ package net.dante;
 
 public class Main {
     void main() {
-        IO.println("Biblioketprogram");
+
+
+            IO.println("""
+            -----Biblioketsystem-----
+            1. Lista bibliotekets böcker
+            2. Lista bibliotekets magasin
+            3. Registrera ny bok i systemet
+            4. Registrera nytt magasin i systemet
+            5. Avsluta programmet
+        """);
+        
     }
 }
