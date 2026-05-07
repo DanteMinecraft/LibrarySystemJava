42ff0c982cea7583d57b0f553d317456a8adb456
DanteMinecraft <dantewallen07@gmail.com>
2026-05-07 22:05:58 +0200

    lade till getters och setters i LibraryItem-klassen

--- src/main/java/net/dante/LibraryItem.java
+++ src/main/java/net/dante/LibraryItem.java
@@ -6,13 +6,35 @@ Klassen är en abstract class för att se till att inga objekt som är endast Li
 utan ett LibraryItem måste vara någon av child-klasserna (dvs BookItem eller MagazineItem).*/
 
 public abstract class LibraryItem {
+
+    // Variabels
     private String id;
     private String title;
     private boolean isAvailable;
 
+    // Constructor for LibraryItem
     public LibraryItem(String id, String title, boolean isAvailable) {
         this.id = id;
         this.title = title;
         this.isAvailable = isAvailable;
     }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    // Setters
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
 }
