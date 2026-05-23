a540ceffd50df470caa5b9e65072fed023202f1d
DanteMinecraft <dantewallen07@gmail.com>
2026-05-23 23:03:56 +0200

    Förenklare meny-klassen genom att lägga till diverse submenu:s

--- src/main/java/net/dante/client/StartMenu.java
+++ src/main/java/net/dante/client/StartMenu.java
@@ -2,7 +2,7 @@ package net.dante.client;
 
 /*Klass som innehåller programmets huvudmeny*/
 
-public class StartMenu { 
+public class StartMenu {
 
     LibraryManager manager;
 
@@ -10,43 +10,69 @@ public class StartMenu {
         this.manager = manager;
     }
 
-    /** 
+    /**
      * @param (IO.readln()
      */
-    public void ShowMenu() { //TODO: Gör att man kan välja att lista endast en sorts föremål i submeny
 
+    public void ShowMenu() {
         IO.println("""
-                        -----Biblioketsystem-----
-                        1. Lista alla föremål
-                        2. Lista alla användare samt avstängda användare
-                        3. Registrera nytt föremål
-                        4. Registrera ny användare
-                        5. Stäng av användare
-                        6. Sök efter föremål
-                        7. Sök efter användare och deras status
-                        8. Ta bort föremål
-                        9. Ta bort användare
-                        10. Låna föremål
-                        11. Lämna tillbaka föremål
-                        12. Avsluta programmet
-                    """);
-            manager.refreshData();
+                -----Biblioketsystem-----
+                    1. Lista alla föremål
+                    2. Hantera användare
+                    3. Registrera nytt föremål
+                    4. Sök efter föremål
+                    5. Ta bort föremål
+                    6. Utlåning
+                    7. Statistik (kommande)
+                    8. Avsluta programmet
+                """);
+        manager.refreshData();
             switch (IO.readln()) {
                 case "1" -> manager.listLibraryItems();
-                case "2" -> manager.listAllUsers();
+                case "2" -> ShowUserMenu();
                 case "3" -> manager.addLibraryItem();
-                case "4" -> manager.addUser();
-                case "5" -> manager.suspendUser();
-                case "6" -> manager.searchItem();
-                case "7" -> manager.searchUser();
-                case "8" -> manager.deleteLibraryItem();
-                case "9" -> manager.deleteUser();
-                case "10" -> manager.borrowItem();
-                case "11" -> manager.returnItem();
-                case "12" -> {
+                case "4" -> manager.searchItem();
+                case "5" -> manager.deleteLibraryItem();
+                case "6" -> ShowLoanMenu();
+                case "7" -> IO.println("Placeholder");
+                case "8" -> {
                     IO.println("Avslutar programmet...");
                     System.exit(0);
                 }
             }
-    }    
+    }
+
+    public void ShowUserMenu() {
+        IO.println("""
+                -----Användarhantering-----
+                    1. Lista alla användare
+                    2. Registrera ny användare
+                    3. Sök efter användare
+                    4. Stäng av användare
+                    5. Ta bort användare
+                    6. Tillbaka
+                """);
+        switch (IO.readln()) {
+            case "1" -> manager.listAllUsers();
+            case "2" -> manager.addUser();
+            case "3" -> manager.searchUser();
+            case "4" -> manager.suspendUser();
+            case "5" -> manager.deleteUser();
+            case "6" -> ShowMenu();
+        }
+    }
+
+    public void ShowLoanMenu() {
+        IO.println("""
+                -----Utlåning-----
+                    1. Låna föremål
+                    2. Lämna tillbaka föremål
+                    3. Tillbaka
+                """);
+        switch (IO.readln()) {
+            case "1" -> manager.borrowItem();
+            case "2" -> manager.returnItem();
+            case "3" -> ShowMenu();
+        }
+    }
 }
